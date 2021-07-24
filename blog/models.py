085897f30e3bfce6d21283dbbfb0b0ccb57faf55
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-07-24 13:43:29 -0400

    fixed slug auto generation

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 STATUS = (
     (0, 'Draft'),
@@ -19,6 +20,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
